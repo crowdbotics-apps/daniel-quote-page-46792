efca32b7999a14da31233e0acb239e0c4b80c3f2
Crowdbotics <team@crowdbotics.com>
2024-03-04 18:09:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "daniel-quote-page-46792",
+  url: "https://daniel-quote-page-46792.botics.co",
+  api: "https://daniel-quote-page-46792.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
